0daf68484c49973852c27d73236c87b97e60a9d6
NaeemAdnan <naeem.cse.nwu@gmail.com>
2024-02-22 11:17:05 +0600

    first commit

---
+++ 1.js
@@ -0,0 +1,14 @@
+let demo = 1;
+ demo = 4;
+console.log(demo);
+
+let hiri = {
+    name: 'ala kala',
+    age: 25
+}
+
+hiri = {
+    name: 'lelo',
+    age: 33
+}
+console.log(hiri);
---
+++ defult.js
@@ -0,0 +1,8 @@
+function add(num1, num2){
+    const result = num1 + num2;
+    console.log(result);
+
+    return result;
+}
+
+const sum = add(2,7)
